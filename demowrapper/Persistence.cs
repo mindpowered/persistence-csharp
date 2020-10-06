46d9fda602260d61bd63b8c85801095dd69784c6
Build <support@mindpowered.ca>
2020-10-06 01:06:00 +0000

    Version 0.0.12

--- demowrapper/Persistence.cs
+++ demowrapper/Persistence.cs
@@ -5,49 +5,68 @@
  */
 
 /**
- * Persistence
+ * Provides a way of storing data for mindpowered packages.
+ * When mindpowered packages need to persist data, they will use Get and Mutate, which in turn will call the Mutators and Getters you have set up.
+ * You can set up the Mutators and Getters however you like whether to access a database such as MySQL or MongoDB, or simply write and read from text files.
  */
 public class Persistence
 {
 
 	/**
-	 * TBD
+	 * Set up a Mutator to change stored data
+	 * @param recordType type of record being changed (eg. databsae table name)
+	 * @param operationName action being performed on the record (eg. insert, update)
+	 * @param strategyMethod method to call to actually perform the mutation
+	 * @param updateMapper method to call on recordData before calling strategyMethod with the results
 	*/
-	AddMutator()
+	AddMutator(recordType, operationName, strategyMethod, updateMapper)
 	{
 		//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
-		//TODO: C# let args = [];
+		//TODO: C# let args = [recordType, operationName, strategyMethod, updateMapper];
 		//TODO: bus.call('Persistence.AddMutator', args);
 	}
 
 	/**
-	 * TBD
+	 * Set up a Getter to retrieve data
+	 * @param recordType type of record being retrieved (eg. databsae table name)
+	 * @param operationName query being performed for the record type (eg. findById, findByName, findActive, getInsertedId)
+	 * @param strategyMethod method to call to actually perform the data retrieval
+	 * @param queryMapper method to call on queryValues before calling strategyMethod with the results
+	 * @param resultMapper method to call on data returned from the strategyMethod before returning the results
 	*/
-	AddGetter()
+	AddGetter(recordType, operationName, strategyMethod, queryMapper, resultMapper)
 	{
 		//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
-		//TODO: C# let args = [];
+		//TODO: C# let args = [recordType, operationName, strategyMethod, queryMapper, resultMapper];
 		//TODO: bus.call('Persistence.AddGetter', args);
 	}
 
 	/**
 	 * TBD
+	 * @param recordType type of record being changed (eg. databsae table name)
+	 * @param operationName action being performed on the record (eg. insert, update)
+	 * @param recordData data being updated or saved by passing through updateMapper and then strategyMethod
 	*/
-	Mutate()
+	Mutate(recordType, operationName, recordData)
 	{
 		//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
-		//TODO: C# let args = [];
+		//TODO: C# let args = [recordType, operationName, recordData];
 		//TODO: bus.call('Persistence.Mutate', args);
 	}
 
 	/**
 	 * TBD
+	 * @param recordType type of record being retrieved (eg. databsae table name)
+	 * @param operationName query being performed for the record type (eg. findById, findByName, findActive, getInsertedId)
+	 * @param queryValues values that will be passed through queryMapper and then strategyMethod to perform the query
+	 * @return {number} The x value.
 	*/
-	Get()
+	Get(recordType, operationName, queryValues)
 	{
 		//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
-		//TODO: C# let args = [];
-		//TODO: bus.call('Persistence.Get', args);
+		//TODO: C# let args = [recordType, operationName, queryValues];
+		//TODO: C# let ret = bus.call('Persistence.Get', args);
+		//TODO: C# return ret;
 	}
 
 }
